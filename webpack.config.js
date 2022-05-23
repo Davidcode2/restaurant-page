c7a7ef0754cff9800fd7be3eb0f8013a4df33078
Jakob Lingel <jlingel68@gmail.com>
2022-05-23 17:50:04 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	devServer: {
+		static: './dist',
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	optimization: {
+		runtimeChunk: 'single',
+	},
+}
