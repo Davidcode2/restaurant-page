42adcafc8d7508ea06b1477a165951c12353760b
Jakob Lingel <jlingel68@gmail.com>
2022-05-23 21:52:03 +0200

    Static site setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style.css';
+import mountain_image from './wp-mountain-clouds.jpg';
+
+console.log("yay");
+console.log("something");
--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,23 @@ module.exports = {
 		static: './dist',
 	},
 	output: {
-		filename: '[name].bundle.js',
+		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist'),
-		clean: true,
 	},
-	optimization: {
-		runtimeChunk: 'single',
-	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+		],
+	}
 }
