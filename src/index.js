6390aa9fab4ebed97e63145a81c88975a085d92e
Jakob Lingel <jlingel68@gmail.com>
2022-05-25 15:36:39 +0200

    Color links

--- src/index.js
+++ src/index.js
@@ -17,11 +17,31 @@ let tabSwitcher = (event) => {
 	console.log(contentToBeAppended);
 	content.removeChild(content.lastChild);
 	content.appendChild(eval(contentToBeAppended));
-	// remove content of current view
-	// render content of chosen view
+	colorItem(event.target);
+}
+
+let colorItem = (elem) => {
+	console.log(elem);
+	if (elem.previousSibling) {
+		elem.previousSibling.style.color = "black";
+		if (elem.previousSibling.previousSibling) {
+			elem.previousSibling.previousSibling.style.color = "black";
+		}
+	}
+	if (elem.nextSibling) {
+		elem.nextSibling.style.color = "black";
+		if (elem.nextSibling.nextSibling) {
+			elem.nextSibling.nextSibling.style.color = "black";
+		}
+	}
+	let parentUl = elem.parentElement;
+	//for (let child in parentUl.children) {
+	//	console.log(child);
+	//	child.style.color = "black";
+	//}
+	elem.style["color"] = "darkgoldenrod";
 }
 
 let header = loadHeader(tabSwitcher)
 content.appendChild(header);
 content.appendChild(menu);
-
